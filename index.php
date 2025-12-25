71c2bbc54748e13e4755508dd81185168c28ebe6
Chamika Jayarathna <failgames.lk@gmail.com>
2025-12-25 19:58:11 +0530

    todo tracker header create

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Full Stack Todo App</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
     <main>
-        <div class="container">
-            <h1>Test</h1>
+      <div class="container">
+        <div class="todo-tracker">
+          <div class="task-tracker-text">
+            <h2>Task Completed</h2>
+            <p class="completed-subheading">Keep it up</p>
+          </div>
+          <div class="task-counter">
+            1 <span class="spacer">/</span> 3
+          </div>
         </div>
+      </div>
     </main>
-    <script src="https://kit.fontawesome.com/2d29da14c6.js" crossorigin="anonymous"></script>
+    <script
+      src="https://kit.fontawesome.com/2d29da14c6.js"
+      crossorigin="anonymous"
+    ></script>
     <script src="./app.js"></script>
-</body>
+  </body>
 </html>
